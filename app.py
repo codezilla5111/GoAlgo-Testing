31bc5c32d3c67035a6aa68e016cd68ae31cc4aa9
Godzilla5111 <singhanurag5111@gmail.com>
2022-05-27 21:09:35 +0530

    changes

--- app.py
+++ app.py
@@ -109,5 +109,6 @@ def page_not_found(e):
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
